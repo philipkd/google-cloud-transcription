5b38dd9bd665ff1bce4810b9cf8b762cf7c6fd42
Philip Dhingra <philipkd@gmail.com>
2022-05-03 17:07:53 -0700

    now has punctuation in there

--- transcribe_streaming_mic.py
+++ transcribe_streaming_mic.py
@@ -197,6 +197,7 @@ def main():
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=RATE,
         language_code=language_code,
+        enable_automatic_punctuation=True,
     )
 
     streaming_config = speech.StreamingRecognitionConfig(
